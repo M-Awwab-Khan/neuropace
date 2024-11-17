36075fb8a9ff8fe027a4ef5ba7bc0431347d60c6
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-11-17 20:35:52 +0500

    refactor: removed unnecessary files

--- assets/data.ts
+++
@@ -1,9 +0,0 @@
-import { GithubIcon } from "@/components/quiz-page/icons";
-
-export const socialLinks = [
-  {
-    id: "GitHub",
-    url: "https://github.com/Evavic44/fun-notes",
-    icon: GithubIcon,
-  },
-];
