c0156a9fabe7f7c6956fd0d740dd05909ebd812c
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-11-17 08:56:09 +0500

    style: decks padding bottom

--- components/decks-page/decks.tsx
+++ components/decks-page/decks.tsx
@@ -153,7 +153,7 @@ export default function Decks({ userId }: { userId: string }) {
           ))}
         </div>
       ) : (
-        <div className="mt-10 flex flex-wrap gap-5">
+        <div className="mt-10 flex flex-wrap gap-5 mb-10">
           {filteredDecks.map(({ id, name, category }) => (
             <DeckCard
               key={id}
