01c587b577557c7ef6c2f43b7ade76abc1bec4ea
MuhammadTalha57 <muhammadtalha.mailme@gmail.com>
2024-11-18 00:56:12 +0500

    bug fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Navbar from "@/components/ui/navbar";
 import { DesktopNavigation } from "@/components/ui/navbar";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as SonnerToaster } from "sonner";
