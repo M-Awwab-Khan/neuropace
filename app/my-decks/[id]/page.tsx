908c7b4733594e1f52753d8d2eee173532741e7f
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-11-18 21:32:03 +0500

    fix: type fixes

--- app/my-decks/[id]/page.tsx
+++ app/my-decks/[id]/page.tsx
@@ -1,4 +1,5 @@
 import Flashcards from "@/components/flashcards-page/flashcards";
+import { Flashcard } from "@/lib/types";
 import { getFlashcards } from "@/lib/actions";
 import { auth } from "@clerk/nextjs/server";
 export default async function FlashcardsPage({
@@ -11,7 +12,7 @@ export default async function FlashcardsPage({
   const { userId, redirectToSignIn } = await auth();
 
   if (!userId) return redirectToSignIn();
-  const flashcards = await getFlashcards(deckId);
+  const flashcards = (await getFlashcards(deckId)) as Flashcard[];
   return (
     <div>
       <Flashcards userId={userId} deckId={deckId} flashcards={flashcards} />
